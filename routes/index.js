5dc2ca3c5032437f2ec5cc64114c9fa47c94c7e2
S566474 <142925730+S566474@users.noreply.github.com>
2023-11-03 14:36:15 -0500

    render

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Door', { title: 'Search Results Door' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
