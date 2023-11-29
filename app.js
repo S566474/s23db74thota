de37cd328fb493d7eef385bc1e966a6fbc6b1720
S566474 <142925730+S566474@users.noreply.github.com>
2023-11-29 10:39:26 -0600

    added

--- app.js
+++ app.js
@@ -87,6 +87,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/resource', resourceRouter);
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,81 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
+var Account = require('./models/account') // Change the variable name here
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// var door_controlers = require('../controllers/DoorController'); // Assuming you have a controller file
+
+// Landing page
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Door App', user: req.user });
 });
+
+// Registration routes
+router.get('/register', function (req, res) {
+  res.render('register', { title: 'Door App Registration' });
+});
+
+router.post('/register', function (req, res) {
+  Account.findOne({ username: req.body.username }) // Change here as well
+    .then(function (user) {
+      if (user != null) {
+        console.log("exists " + req.body.username);
+        return res.render('register', { title: 'Registration', message: 'Existing User', account: req.body.username });
+      }
+
+      let newAccount = new Account({ username: req.body.username }); // Change here as well
+      Acc.register(newAccount, req.body.password, function (err, user) { // Change here as well
+        if (err) {
+          console.log("db creation issue " + err);
+          return res.render('register', { title: 'Registration', message: 'access error', account: req.body.username });
+        }
+
+        if (!user) {
+          return res.render('register', { title: 'Registration', message: 'access error', account: req.body.username });
+        }
+
+        console.log('Success, redirect');
+        res.redirect('/');
+      });
+    })
+    .catch(function (err) {
+      return res.render('register', { title: 'Registration', message: 'Registration error', account: req.body.username });
+    });
+});
+
+// Login routes
+router.get('/login', function (req, res) {
+  res.render('login', { title: 'Door App Login', user: req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  res.redirect('/');
+});
+
+// Logout route
+router.get('/logout', function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
+// Ping route
+router.get('/ping', function (req, res) {
+  res.status(200).send('pong!');
+});
+
+// Protected route - Update door page
+router.get('/update', ensureAuthenticated, door_controlers.door_update_Page);
+
+// Middleware to ensure authentication
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 module.exports = router;
