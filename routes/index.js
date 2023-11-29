f3a73058470b65ec8c8dcf4b5937f800a6a12d63
S566474 <142925730+S566474@users.noreply.github.com>
2023-11-29 14:41:12 -0600

    added

--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,56 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
-
 var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 var Account = require('../models/account');
-
 router.get('/', function (req, res) {
   res.render('index', {
     title: 'Badminton App',
     user: req.user
   });
 });
-
 router.get('/register', function (req, res) {
   res.render('register', {
     title: 'Badminton App Registration'
   });
 });
-
-router.post('/register', async function (req, res) {
+router.post('/register', async function(req, res) {
+ 
   try {
-    const user = await Account.findOne({ username: req.body.username }).exec();
-    if (user) {
-      return res.render('register', {
-        title: 'Registration',
-        message: 'Existing User',
-        account: req.body.username
-      });
-    }
-
-    let newAccount = new Account({ username: req.body.username });
-    const registeredUser = await Account.register(newAccount, req.body.password);
-    console.log('Success, redirect');
-    res.redirect('/');
-  } catch (err) {
-    console.error(err);
-    return res.render('register', {
-      title: 'Registration',
-      message: 'Registration error',
-      account: req.body.username
-    });
+  const user = await Account.findOne({ username : req.body.username }).exec();
+  if(user){
+  return res.render('register', { title: 'Registration',
+  message: 'Existing User', account : req.body.username });
   }
-});
-
+  let newAccount = new Account({ username : req.body.username });
+  const registeredUser = await Account.register(newAccount, req.body.password);
+  console.log('Success, redirect');
+  res.redirect('/');
+  } catch (err) {
+  console.error(err);
+  return res.render('register', { title: 'Registration',
+  message: 'Registration error', account : req.body.username });
+   }
+ 
+  });
 router.get('/login', function (req, res) {
   res.render('login', {
     title: 'Badminton App Login',
     user: req.user
   });
 });
-
 router.post('/login', passport.authenticate('local'), function (req, res) {
-  if (req.session.returnTo) {
+  if (req.session.returnTo)
     res.redirect(req.session.returnTo);
-  } else {
-    res.redirect('/');
-  }
+  res.redirect('/');
 });
-
-router.get('/logout', function (req, res, next) {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect('/');
+router.get('/logout', function(req, res, next) {        
+  req.logout( function(err) {
+      if (err) { return next(err);}
+      res.redirect('/');
   });
 });
-
 router.get('/ping', function (req, res) {
   res.status(200).send("pong!");
 });
-
 module.exports = router;
+ 
