aa37d6ca160077b689eff202353889041cd63d6a
S566474 <142925730+S566474@users.noreply.github.com>
2023-11-29 16:21:18 -0600

    added

--- routes/resource.js
+++ routes/resource.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+// Require controller modules.
 var api_controller = require('../controllers/api');
-var door_controller = require('../controllers/Door');
+var door_controller = require('../controllers/door');
+/// API ROUTE ///
+// GET resources base.
 router.get('/', api_controller.api);
+/// door ROUTES ///
+// POST request for creating a door.
 router.post('/door', door_controller.door_create_post);
+// DELETE request to delete door.
 router.delete('/door/:id', door_controller.door_delete);
+// PUT request to update door.
 router.put('/door/:id', door_controller.door_update_put);
+// GET request for one door.
 router.get('/door/:id', door_controller.door_detail);
+// GET request for list of all door items.
 router.get('/door', door_controller.door_list);
-
-
-
 module.exports = router;
